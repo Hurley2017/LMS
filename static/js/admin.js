9f4a9c25f0ac65e2aac516613dd335c2e78f88c6
Hurley2017 <luciefer9062hurley@gmail.com>
2023-05-28 13:47:30 +0530

    a lot changed!

---
+++ static/js/admin.js
@@ -0,0 +1,19 @@
+document.getElementById('upload-form').addEventListener('submit', function(e) {
+    e.preventDefault(); // Prevent form submission
+  
+    var fileInput = document.getElementById('file-input');
+    var file = fileInput.files[0]; // Get the selected file
+  
+    if (file) {
+      var reader = new FileReader();
+  
+      reader.onload = function(e) {
+        var contents = e.target.result;
+        // Process the file contents here
+        console.log('File contents:', contents);
+      };
+  
+      reader.readAsText(file); // Read the file as text
+    }
+  });
+  
