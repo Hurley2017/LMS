569522a751d68e4ae5a28f189337e094b79e0a15
Hurley2017 <luciefer9062hurley@gmail.com>
2023-05-28 22:49:45 +0530

    now book information render on screen

---
+++ static/js/logout.js
@@ -0,0 +1,10 @@
+function clearCookies(vr) {
+    document.cookie = vr + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  }
+  function logout()
+  {
+    clearCookies("email");
+    clearCookies("session");
+    window.location = "/login" 
+  }
+   
